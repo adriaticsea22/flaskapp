29ce5a6c7ec869c4f5965cb99b8c687d92e6a2cd
adriaticsea22 <adrian89987@gmail.com>
2019-03-10 18:53:34 -0400

    new feature

--- logDHT2.py
+++ logDHT2.py
@@ -4,27 +4,51 @@
 #  logDHT.py
 #
 #  Developed by Marcelo Rovai, MJRoBot.org @ 10Jan18
-#  
+#
 #  Capture data from a DHT22 sensor and save it on a database
 
 import time
 import sqlite3
 import Adafruit_DHT
+import serial
 
 dbname='sensorsData.db'
 sampleFreq = 60 # time in seconds
 
 # get data from DHT sensor
-def getDHTdata():	
-	
+def getDHTdata():
+
 	DHT22Sensor = Adafruit_DHT.DHT22
 	DHTpin = 16
 	hum, temp = Adafruit_DHT.read_retry(DHT22Sensor, DHTpin)
-	
+
 	if hum is not None and temp is not None:
 		hum = round(hum)
 		temp = round(temp, 1)
 	return temp, hum
+
+def getCircuitdata():
+	try:
+		ser = serial.Serial('/dev/ttyACM0', 9600)
+	except SerialException:
+		pass
+	try:
+		ser = serial.Serial('/dev/ttyACM1', 9600)
+	except SerialException:
+		pass
+	try:
+		ser = serial.Serial('/dev/ttyACM2', 9600)
+	except SerialException:
+		pass
+	try:
+		ser = serial.Serial('/dev/ttyACM3', 9600)
+	except SerialException:
+		ser = serial.Serial('/dev/ttyACM4', 9600)
+
+	ser.reset_input_buffer()
+	lightlevel = float(ser.readline().decode())
+	return lightlevel
+
 
 # log sensor data on database
 
@@ -34,6 +58,10 @@ temp, hum = getDHTdata()
 conn=sqlite3.connect(dbname)
 curs=conn.cursor()
 curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?))", (temp, hum))
+try:
+	lightlevel = getCircuitdata()
+	curs.execute("INSERT INTO circuit_data values(datetime('now'), (?)", (lightlevel,))
+except:
+	pass
 conn.commit()
 conn.close()
-
