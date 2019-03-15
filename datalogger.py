d8723154a8b6cf73f644c60ac1d3add4b22ef2a7
adriaticsea22 <adrian89987@gmail.com>
2019-03-14 20:05:51 -0400

    dev

--- datalogger.py
+++ datalogger.py
@@ -9,7 +9,7 @@ import adafruit_veml6075
 i2c = busio.I2C(board.SCL, board.SDA)
 veml = adafruit_veml6075.VEML6075(i2c, integration_time=800)
 lux = adafruit_tsl2591.TSL2591(i2c)
-
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 def uv_read():
     UV_index = veml.uv_index
@@ -23,15 +23,24 @@ def lux_read():
     infrared = lux.infrared
     return light_level, visible_light, infrared
 
+def bme_read():
+    temperature = bme280.temperature * 1.8 + 32
+    humidity = bme280.humidity
+    pressure = bme.pressure
+    return temperature, humidity, pressure
 
 
 def log_data():
     UV_index, UV_A, UV_B = uv_read()
     light_level, visible_light, infrared = lux_read()
+    temperature, humidity, pressure = bme_read()
     aio.send('uv-index', UV_index)
     aio.send('uv-a', UV_A)
     aio.send('uv-b', UV_B)
     aio.send('lux', visible_light)
+    aio.send('indoor-temperature', temperature)
+    aio.send('indoor-humidity', humidity)
+    aio.send('indoor-pressure', pressure)
     print('success')
 
 log_data()
