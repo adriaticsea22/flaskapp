e7f3e30b639e98bfd66e26def260f4e43435265f
Adrian Amador <adrian.amador@snapdragonchemistry.com>
2019-04-15 22:50:30 -0400

    typo

--- datalogger.py
+++ datalogger.py
@@ -16,8 +16,6 @@ import requests
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-client = InfluxDBClient(host='192.168.1.179', port=8086, database='sensordb')
-
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
